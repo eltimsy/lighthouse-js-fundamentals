aac65c2017eafd4ae62999f746adbf006ffe8317
Timothy Tse <timothy.sc.tse@gmail.com>
2016-08-08 14:40:51 +0000

    Adds odd and age-calculator function exercises

---
+++ ageCalculator.js
@@ -0,0 +1,7 @@
+function ageCalculator(name, yearOfBirth, currentYear){
+  return(name + "is " + (currentYear - yearOfBirth) + " years old.");
+}
+
+console.log(ageCalculator("Jay", 1983, 2016));
+console.log(ageCalculator("Megan", 2000, 2016));
+console.log(ageCalculator("Kelly", 2010, 2016));
