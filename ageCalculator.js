4b5e8367bcd2f274616ddf8a8bb065a437ad8080
Timothy Tse <timothy.sc.tse@gmail.com>
2016-08-08 14:47:15 +0000

    Adds some changes to age-cal

--- ageCalculator.js
+++ ageCalculator.js
@@ -1,5 +1,5 @@
 function ageCalculator(name, yearOfBirth, currentYear){
-  return(name + "is " + (currentYear - yearOfBirth) + " years old.");
+  return(name + " is " + (currentYear - yearOfBirth) + " years old.");
 }
 
 console.log(ageCalculator("Jay", 1983, 2016));
